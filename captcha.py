e07981d8632b5f0ff25893dbba02b64f476fdb32
SevenNine <68588725+SevenNine233@users.noreply.github.com>
2026-02-01 15:53:56 +0800

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -1,10 +1,57 @@
-from request import http
+import httpx
+import time
+
+
+class ttocr:
+    def recognize(gt: str, challenge: str):
+        submit_url = "http://api.ttocr.com/api/recognize"
+        params = {
+            "appkey": "9f4aace9e81822ef9906bedfcb931149",
+            "gt": gt,
+            "challenge": challenge,
+            "itemid": "388",
+            "referer": 'https://act.mihoyo.com'
+        }
+        for _ in range(10):
+            res = httpx.post(submit_url, data=params)
+            result = res.json()
+            if result.get("status") == 1:
+                return result.get("resultid")
+            time.sleep(2)
+        return None
+
+
+    def results(resultid: str):
+        result_url = "http://api.ttocr.com/api/results"
+        params = {
+            "appkey": "9f4aace9e81822ef9906bedfcb931149",
+            "resultid": resultid
+        }
+        if resultid == "":
+            return None
+        else:
+            for _ in range(10):
+                res = httpx.post(result_url, data=params)
+                data = res.json()
+                if data.get("status") == 1:
+                    datajson = {
+                        "challenge":data["data"]["challenge"],
+                        "validate":data["data"]["validate"]
+                    }
+                    return datajson
+                time.sleep(2)
+            return None
 
 
 def game_captcha(gt: str, challenge: str) -> dict:
-    # challenge不要直接用传入的，老格式也还支持，但是建议优先使用新格式
-    return None  # 失败返回None 成功返回{"challenge":challenge,"validate":validate}
+    resultid = ttocr.recognize(gt, challenge)
+    time.sleep(2)
+    data = ttocr.results(resultid)
+    return data
 
 
 def bbs_captcha(gt: str, challenge: str) -> dict:
-    return None
+    resultid = ttocr.recognize(gt, challenge)
+    time.sleep(2)
+    data = ttocr.results(resultid)
+    return data
